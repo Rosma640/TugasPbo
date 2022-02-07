e802e465306245d4cdaa39b235110575b52dfb68
Rosmawati <90950041+Rosma640@users.noreply.github.com>
2022-02-07 18:24:37 +0700

    Create BangunDatar.java

---
+++ BangunDatar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bangundatar;
+
+/**
+ *
+ * @author Rsm
+ */
+public class BangunDatar {
+    float luas(){
+        System.out.println("Luas bangun datar dan");
+        return 0;
+    }
+    
+    float keliling(){
+        System.out.println("Keliling bangun datar \n");
+        return 0;
+    }
+}
